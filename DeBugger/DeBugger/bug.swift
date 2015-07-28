3fc955c6aa7304bf3e36bc5bc7e407382a205239
ekwan103 <ekwan103@bu.edu>
2015-07-28 15:01:13 -0400

    made the file for a bug class

---
+++ DeBugger/DeBugger/bug.swift
@@ -0,0 +1,13 @@
+//
+//  bug.swift
+//  DeBugger
+//
+//  Created by Eugene Kwan on 7/28/15.
+//  Copyright (c) 2015 EC327teamBug. All rights reserved.
+//
+
+import Foundation
+
+class bug {
+    
+}
