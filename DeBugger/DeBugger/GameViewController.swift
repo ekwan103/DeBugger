ff42628bd724d9b0cd963839cb814d47a4209596
ekwan103 <ekwan103@bu.edu>
2015-07-29 12:21:06 -0400

    added BugGame

--- DeBugger/DeBugger/GameViewController.swift
+++ DeBugger/DeBugger/GameViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class GameViewController: UIViewController {
     
+    let container = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // set container frame and add to the screen
+        self.container.frame = CGRect(x: 0, y: 75, width: 375, height: 592)
+        self.container.backgroundColor = UIColor.purpleColor()
+        self.view.addSubview(container)
     }
     
     override func didReceiveMemoryWarning() {
